acce884fcb359109ae01647257e4d9185208fb2b
eilyri <khaeun1029@gmail.com>
2022-04-30 02:21:45 +0900

    [fix] HEADERS 이름 수정

--- BlindCafe/Configuration/Constant.swift
+++ BlindCafe/Configuration/Constant.swift
@@ -10,5 +10,5 @@ import Alamofire
 struct Constant {
     static let BASE_URL = "https://www.blindcafe.me"
     
-    static var HEADERS: HTTPHeaders = ["x-access-token": UserDefaults.standard.string(forKey: "UserJwt") ?? ""]
+    static var HEADERS: HTTPHeaders = ["Authorization": UserDefaults.standard.string(forKey: "UserJwt") ?? ""]
 }
