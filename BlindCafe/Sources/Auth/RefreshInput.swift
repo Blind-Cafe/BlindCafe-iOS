688eebf6598811a9ae19183cfe3a37718af52095
eilyri <khaeun1029@gmail.com>
2022-04-30 02:23:38 +0900

    [feat] 토큰 갱신 input, response 파일 추가

---
+++ BlindCafe/Sources/Auth/RefreshInput.swift
@@ -0,0 +1,12 @@
+//
+//  RefreshInput.swift
+//  BlindCafe
+//
+//  Created by 권하은 on 2022/04/27.
+//
+
+import Foundation
+
+struct RefreshInput: Encodable {
+    var refreshToken: String
+}
---
+++ BlindCafe/Sources/Auth/RefreshResponse.swift
@@ -0,0 +1,12 @@
+//
+//  RefreshResponse.swift
+//  BlindCafe
+//
+//  Created by 권하은 on 2022/04/27.
+//
+
+import Foundation
+
+struct RefreshResponse: Decodable {
+    var accessToken: String
+}
